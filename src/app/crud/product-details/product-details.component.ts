89c182b6871ede9ad7afe033ff2d56b82897dbb7
Bingomar <ga19033@ues.edu.sv>
2025-06-07 21:27:06 -0600

    Metodo de ver creado

--- src/app/crud/product-details/product-details.component.ts
+++ src/app/crud/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CRUDService } from 'src/app/services/crud.service';
+import { Product } from '../models/product';
+
 
 @Component({
   selector: 'app-product-details',
@@ -7,10 +10,28 @@ import { CRUDService } from 'src/app/services/crud.service';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  //@ts-ignore
+  detalleProducto: Product;
 
-  constructor(private crudService : CRUDService) { }
+  constructor(private crudService: CRUDService,
+              private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    let productId = '';
+    if (this.activatedRoute.snapshot.params['productoId']) {
+      productId = this.activatedRoute.snapshot.params['productoId'];
+      if (productId !== '') {
+        this.loadProductDetails(productId);
+
+      }
+    }
+  }
+
+  loadProductDetails(productId: any) {
+    this.crudService.loadProductInfo(productId).subscribe(res => {
+      this.detalleProducto = res;
+    });
   }
 
 }
