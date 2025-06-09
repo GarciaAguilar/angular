c2e843c44250170d3d9d1c6c1b2b2c47aea1cab3
Bingomar <ga19033@ues.edu.sv>
2025-06-08 18:03:31 -0600

    Actualizando metodos de ver y eliminar

--- src/app/crud/models/http-response.ts
+++ src/app/crud/models/http-response.ts
@@ -1,5 +1,5 @@
 export interface HttpResponse {
     result: string;
     message: string;
-    data?: any[]; // Cambiado a any[] para permitir un array de datos
+    data?: any[]; 
 }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -10,19 +10,15 @@ import { Product } from '../crud/models/product';
 })
 export class CRUDService {
 
-  constructor(private httpClient : HttpClient) { }
-  
-  loadProducts() {
+  constructor(private httpClient: HttpClient) { }
+
+  //Metodo para cargar los productos
+  loadProducts(): Observable<Product[]> {
     const url = environment.API_EndPoint + 'listar.php';
-    return this.httpClient.get(url).pipe(map(data => data));
-  }
-/*loadProducts(): Observable<Product[]> {  // Cambiado a Observable<Product[]>
-    const url = environment.API_EndPoint + 'listar.php';
-    
+
     return this.httpClient.get<HttpResponse>(url).pipe(
       map(response => {
         if (response.result === 'success') {
-          // Asegúrate de que response.data existe y es un array
           return response.data || [];
         }
         throw new Error(response.message || 'Error desconocido');
@@ -32,28 +28,32 @@ export class CRUDService {
         throw error;
       })
     );
-  }*/
+  }
 
   //Metodo para crear un producto
- 
+
   createProduct(formData: FormData): Observable<HttpResponse> {
     const url = environment.API_EndPoint + 'crear.php';
     return this.httpClient.post<HttpResponse>(url, formData);
   }
 
+  //Metodo para cargar un producto por su id
 
   loadProductInfo(productId: any): Observable<Product> {
-    const url = environment.API_EndPoint + 'view_one.php?id=' + productId;
+    const url = environment.API_EndPoint + 'listar_uno.php?id=' + productId;
     return this.httpClient.get<Product>(url).pipe(map(data => data));
   }
-  
+
+  //Metodo para editar un producto
   editProduct(data: any): Observable<HttpResponse> {
     const url = environment.API_EndPoint + 'actualizar.php';
     return this.httpClient.post<HttpResponse>(url, data).pipe(map(data => data));
   }
 
+  //Metodo para eliminar un producto
+
   deleteProduct(productId: any): Observable<HttpResponse> {
-    const url = environment.API_EndPoint + 'delete.php?id=' + productId;
+    const url = environment.API_EndPoint + 'eliminar.php?id=' + productId;
     return this.httpClient.get<HttpResponse>(url).pipe(map(data => data));
   }
 }
